a94f639e05032c5aa57d5d103b80f6cc79ec2fba
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-12-04 17:02:57 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -12,18 +12,47 @@ class Person(Base):
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    type_of_favorite = Column(String(250), nullable=False)
+    people_id= Column(Integer, ForeignKey('people.id'), nullable=True)
+    planet_id= Column(Integer, ForeignKey('planets.id'), nullable=True)
+    starship_id= Column(Integer, ForeignKey('starships.id'), nullable=True)
+
+class People(Base):
+    __tablename__ = 'people'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    birth_year = Column(String(250))
+    gender = Column(String(250))
+    hair_color = Column(String(250))
+    eye_color = Column(String(250))
+    skin_color = Column(String(250))
+   
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    gravity = Column(Integer)
+    population = Column(Integer)
+    diameter = Column(Integer)
+   
 
     def to_dict(self):
         return {}
